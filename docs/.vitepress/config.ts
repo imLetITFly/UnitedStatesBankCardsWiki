a4b0839d1d001ad618e2fca0870a7792b4813db2
imLetITFly <53560533+imLetITFly@users.noreply.github.com>
2025-02-16 23:53:03 -0500

    Create config.ts
    from https://vitepress.dev/guide/getting-started

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitepress';
+
+// refer https://vitepress.dev/reference/site-config for details
+export default defineConfig({
+  lang: 'en-US',
+  title: 'UnitedStatesBankCardsWiki',
+  description: 'From ITIN to SSN, from B/F/J/H/L/O Visa to Green Card/Citizen, we all have legal pathway to U.S. Bank services such as checking account, debit card and credit card.',
+
+  themeConfig: {
+    nav: [
+      { text: 'vitepress Example', link: '/vitepressExample' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        // text: 'Guide',
+        items: [
+          { text: 'vitepress Example', link: '/vitepressExample' },
+          // ...
+        ],
+      },
+    ],
+  },
+});
