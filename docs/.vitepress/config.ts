6a40c9917b88298369d9c9766581e6de8ff56187
imLetITFly <53560533+imLetITFly@users.noreply.github.com>
2025-02-17 00:08:09 -0500

    Update config.ts: fix grammar error

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   lang: 'en-US',
   title: 'UnitedStatesBankCardsWiki',
   description: 'From ITIN to SSN, from B/F/J/H/L/O Visa to Green Card/Citizen, we all have legal pathway to U.S. Bank services such as checking account, debit card and credit card.',
-  base: /UnitedStatesBankCardsWiki/
+  base: '/UnitedStatesBankCardsWiki/',
   themeConfig: {
     nav: [
       { text: 'vitepress Example', link: '/vitepressExample' },
